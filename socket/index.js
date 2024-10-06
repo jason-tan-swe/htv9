65ecc8e44057e67bc73c75bb3804ceb50f43b2ce
Jason Tan <jaskyle4@gmail.com>
2024-10-06 02:11:15 -0400

    chore: fix deploy sockets

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8080';
+const URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:8080';
 
 export const socket = io(URL);
